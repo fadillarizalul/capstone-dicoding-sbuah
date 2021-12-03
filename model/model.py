33266e97835635ae69512d72f086ef2ad6759d87
Fadilla Rizalul <76253393+fadillarizalul@users.noreply.github.com>
2021-12-03 10:52:46 +0700

    Update model.py with add preprocess of data
    Image augmentation with image data generator and split data

--- model/model.py
+++ model/model.py
@@ -34,3 +34,37 @@ print("Amount of fresh orange images:", fresh_orange)
 print("Amount of rotten banana images:", rotten_banana)
 print("Amount of rotten mango images:", rotten_mango)
 print("Amount of rotten orange images:", rotten_orange)
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+ 
+train_datagen = ImageDataGenerator(
+                    rescale=1./255,
+                    rotation_range=0.2,
+                    width_shift_range=0.2,
+                    height_shift_range=0.2, 
+                    horizontal_flip=True,
+                    shear_range = 0.2,
+                    zoom_range=0.2,
+                    fill_mode = 'wrap',
+                    validation_split=0.2
+                    )
+
+train_generator = train_datagen.flow_from_directory(
+        dir_base, # direktori data base
+        target_size=(150, 100),  # mengubah resolusi seluruh gambar menjadi 150x100 piksel
+        batch_size=4,
+        color_mode='rgb',
+        subset='training',
+        # klasifikasi > 2 kelas maka menggunakan class_mode = 'categorical'
+        class_mode="categorical"
+        )
+ 
+validation_generator = train_datagen.flow_from_directory(
+        dir_base, # direktori data base
+        target_size=(150, 100), # mengubah resolusi seluruh gambar menjadi 150x100 piksel
+        batch_size=4,
+        color_mode='rgb',
+        subset='validation', 
+        # klasifikasi > 2 kelas maka menggunakan class_mode = 'categorical'
+        class_mode='categorical'
+        )
